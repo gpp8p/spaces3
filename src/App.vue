950359330b626becf4ad2b1a795bdeb62e44d7a0
George Pipkin <gpp8p@Virginia.EDU>
2024-04-07 11:23:49 -0400

    Entry frame

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
-<script setup lang="ts">
-//import gridTest from "./components/gridTest.vue";
-//import myMenu from "./components/menu.vue";
-//import dropdown from "./components/drop.vue";
-//import htmlTextInputTest from './components/htmlTextInputTest.vue';
-//import menuItemTest from './components/menuItemTest.vue';
-//import menuBarTest from './components/menuBarTest.vue';
-import dynamicMenuTest from './components/dynamicMenuTest.vue';
-//import menuItemDropTest from "./components/menuItemDropTest.vue";
-</script>
 <template>
-  <span  style = "height:100%;" class="w-screen">
-    <dynamicMenuTest></dynamicMenuTest>
-
-  </span>
+  <div class="flex flex-col h-screen p-1vh">
+    <div class="w-98vw h-14-5vh mx-auto mb-1vh border-2 border-blue-500 bg-orange-100 rounded-lg"></div>
+    <div class="w-98vw h-84-5vh mx-auto border-2 border-blue-500 bg-orange-100 rounded-lg"></div>
+  </div>
 </template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+
+const resizeHandler = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+onMounted(() => {
+  resizeHandler();
+  window.addEventListener('resize', resizeHandler);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', resizeHandler);
+});
+</script>
+
+<style scoped>
+.h-14-5vh {
+  height: calc(var(--vh, 1vh) * 14.5);
+}
+
+.h-84-5vh {
+  height: calc(var(--vh, 1vh) * 84.5);
+}
+
+.w-98vw {
+  width: 98vw;
+}
+
+.p-1vh {
+  padding-top: calc(var(--vh, 1vh) * 1);
+  padding-bottom: calc(var(--vh, 1vh) * 1);
+}
+
+.mb-1vh {
+  margin-bottom: calc(var(--vh, 1vh) * 1);
+}
+</style>
