3e8fb2e22224994da54dc715cba8c3826a5e30c6
George Pipkin <gpp8p@Virginia.EDU>
2024-10-20 22:12:56 -0400

    more work on card configure

--- src/components/dialogDefinitions2.js
+++ src/components/dialogDefinitions2.js
@@ -357,22 +357,26 @@ const defs = function(dialogDef){
 //                    const store = useLogStateStore();
                     const loginResult= toRaw(loginStore.loginStatus);
                     console.log('loginResult dialog4 - ', loginResult);
+
                     const parms   = {
-                        orgId:loginResult.orgId,
-                        userId:loginResult.userName,
-                        layoutId:loginResult.orgHome,
+                        cardId:config.id,
+                        layoutId:config.layoutId
                     }
                     const {executeTrans} = getTrans();
                     const header = loginResult.access_token;
                     const dataReady = ref(false);
                     const transResult = ref({});
                     console.log('in card configure dialog data - config is-', config);
-                    params:{
-                        cardId:config.id
-                        layoutId:config.layoutId
-                    }
-                    //executeTrans(parms, c.ALL_PAGES,  c.API_PATH+'api/shan/getCardDataById?XDEBUG_SESSION_START=19884', 'GET', emit, c, header, dataReady, transResult);
+                    console.log('parms are-', parms);
+                    executeTrans(parms, c.CARD_MENUS_CONFIGURE,  c.API_PATH+'api/shan/getCardDataById?XDEBUG_SESSION_START=19884', 'GET', emit, c, header, dataReady, transResult);
+                    whenever(dataReady, () => {
+                        debugger;
+                        console.log('card configuration data is ready-', transResult._rawValue);
+                        ready.value=true;
+                        result.value = {
 
+                        }
+                    })
                 },
                 addActions:function(currentFuncs) {
 
